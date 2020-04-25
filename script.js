e0f056822b3b396daa0db3e87c4431db9b1e0105
jdbogle1 <jam.d.bogle@gmail.com>
2020-04-25 19:03:14 +0000

    Added: delete command Changed: help command

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var mainArg;
 var split;
 var cLen = 10;
 var blStat = 0;
-var numLines = 1;
+var copyID = 0;
 var pInput;
 var lineNew;
 var lineOld;
@@ -21,7 +21,8 @@ const comSynt = ['log.[Text].[Storage Tag].[ID Number(optional)]',
     'edit.[Edit type].[Storage Tag].[ID Number]',
     'del.[Storage Tag].[ID Number]',
     'copy.[Storage Tag].[ID Number]',
-    'google.[Storage Tag].[ID Number]'];
+    'google.[Storage Tag].[ID Number]',
+    'format.[Format Type].[Format Style]'];
 const errInp = "The function you have entered is not recognized or you have used the incorrect syntax. Type 'help' for a list of available functions.";
 const errRead = ["Please enter a valid ID or 'all' to read a stored note.", "There isn't a note stored with the tag and ID you entered, please try again."];
 const errUnk = "Encountered an unknown error. Aborting the command..."
@@ -93,10 +94,14 @@ function process(flag) {
         log(cInput);
     } else if (mainFunc == 'read') {
         read(cInput);
+    } else if (mainFunc == 'del') {
+        del(cInput);
     } else if (mainFunc == 'help') {
         help(cInput);
-    } else if (mainFunc=='copy'){
+    } else if (mainFunc == 'copy') {
         copy(cInput);
+    } else if (mainFunc == 'google') {
+        google(cInput);
     } else {
         //Occurs when the user inputs an unrecognised command
         funcLine(errInp, msgType[2]);
@@ -144,26 +149,41 @@ function log(flag) {
     console.log(localStorage.getItem(tag));
 }
 function help(flag) {
-    for (var i = 0; i < comName.length; i++) {
-        var command = comName[i];
-        syntax =
-            clas = 'output'
-        funcLine(command, clas)
+    com = flag.split('.')[1];
+    if (flag.split('.').length == 2) {
+        if (com == 'log') {
+            var helpText = "The syntax for the command you entered is: ";
+            funcLine(helpText, msgType[1]);
+            funcLine(comSynt[0],msgType[1]);
+        }
+        for (var i = 0; i < comName.length; i++) {
+            if (flag.split('.')[1] == i) {
+                var helpText = "The syntax for the command you entered is: " + comSynt[i];
+                funcLine(helpText, msgType[1])
+            }
+        }
+    } else {
+        for (var i = 0; i < comName.length; i++) {
+            var command = comName[i];
+            syntax =
+                clas = 'output'
+            funcLine(command, clas)
+        }
     }
+
 }
 function read(flag) {
     var gotText;
     tag = flag.split('.')[1];
     index = parseInt(flag.split('.')[2]);
     if (Number.isNaN(index) && flag.split('.')[2] == "all") {
-        gotText="Retrived notes from: "+tag;
-        funcLine(gotText,msgType[1]);
-        for (var i = 0; i <= parseInt(localStorage.getItem(tag)); i++){
-            storageKey=tag+i.toString();
-            text=localStorage.getItem(storageKey);
-            funcLine(text,msgType[1])
+        gotText = "Retrived notes from: " + tag;
+        funcLine(gotText, msgType[1]);
+        for (var i = 0; i <= parseInt(localStorage.getItem(tag)); i++) {
+            storageKey = tag + i.toString();
+            text = localStorage.getItem(storageKey);
+            funcLine(text, msgType[1])
         }
-            console.log("if");
     } else if (Number.isInteger(index)) {
         if (localStorage.getItem(tag) == null) {
             funcLine(errRead[1], msgType[2]);
@@ -182,32 +202,65 @@ function edit(flag) {
 
 }
 function del(flag) {
+    var delText;
+    tag = flag.split('.')[1];
+    index = parseInt(flag.split('.')[2]);
+    if (Number.isNaN(index) && flag.split('.')[2] == "all") {
+        delText = "Deleted all notes from: " + tag;
+        funcLine(delText, msgType[1]);
+        for (var i = 0; i <= parseInt(localStorage.getItem(tag)); i++) {
+            storageKey = tag + i.toString();
+            localStorage.removeItem(storageKey);
+            localStorage.removeItem(tag);
+        }
+    }
+    else if (Number.isInteger(index)) {
+        if (localStorage.getItem(tag) == null) {
+            funcLine(errRead[1], msgType[2]);
+        } else {
+            storageKey = tag + index.toString();
+            localStorage.removeItem(storageKey);
+            text = localStorage.getItem(storageKey);
+            delText = "Deleted note from: " + storageKey;
+            funcLine(gotText, msgType[1]);
+        }
+    } else {
+        funcLine(errRead[0], msgType[2]);
+    }
 
 }
 function copy(flag) {
     tag = flag.split('.')[1];
     index = flag.split('.')[2];
-    storageKey=tag+index;
-    funcLine("Copied:",msgType[1]);
-    
-    lineOld = document.createElement("p");
-    textOld = document.createTextNode(text)
-    lineOld.append(textOld);
-    lineOld.setAttribute("class", clas)
+    storageKey = tag + index;
+    funcLine("Copied: " + localStorage.getItem(storageKey), msgType[1]);
+    var tempInput = document.createElement("input");
+    tempInput.value = localStorage.getItem(storageKey);
     mainDiv = document.getElementById("main");
-    mainDiv.appendChild(lineOld);
-
-    text=localStorage.getItem(storageKey);
-    text.select()
+    mainDiv.appendChild(tempInput);
+    tempInput.select();
     document.execCommand("copy");
+    mainDiv.removeChild(tempInput);
 }
-function google() {
+function google(flag) {
+    // tag = flag.split('.')[1];
+    // index = flag.split('.')[2];
+    // storageKey = tag + index;
+    // var gText=localStorage.getItem(storageKey);
+    // gArray=gText.split("")
+    // var qURL=""
+    // for(var i=0;i<gArray.length();i++){
+    //     if (gArray==" "){
+
+    //     }
+    // }
+    // gURL="https://www.google.com/search?q="+
+    // window.open("https://www.w3schools.com");
+}
+function remind() {
 
 }
-function remind(){
-
-}
-function format(){
+function format() {
 
 }
 
